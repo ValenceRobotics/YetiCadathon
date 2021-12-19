b635b9a59c56e0f28bf1efdc698fa6778a232034
Jacob <jacobvm04@gmail.com>
2021-12-18 23:09:20 -0500

    Drivetrain simulation stuff

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -17,9 +17,24 @@ public final class Constants {
     public static final int kMotor2Port = 1;
     public static final int kMotor3Port = 2;
     public static final int kMotor4Port = 3;
+    public static final int kGyroPort = 1;
+    public static final int kEncoderTicksPerRev = 4096;
+    public static final double kSensorGearRatio = 1;
+    // Inches
+    public static final double kWheelRadius = 3;
+    public static final int k100msPerSecond = 10;
+    // in kg
+    public static final double kRobotMOI = 2;
+    // in kg
+    public static final double kRobotMass = 26;
+    // Meters
+    public static final double kDistanceBetweenWheels = 0.5;
+    public static final int kMotorsPerSide = 2;
+    public static final double kGearRatio = 10.71;
+    public static final double kSimDelta = 0.02;
 
     public static final class OI {
         public static final int kJoystick1Port = 0;
-        public static final int kJoystick2Port = 0;
+        public static final int kJoystick2Port = 1;
     }
 }
